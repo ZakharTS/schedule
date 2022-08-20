2d46fd8d457bb6e3bb9295e79b522adcc378d8a5
Zakhar <zaharsehar@gmail.com>
2022-08-21 00:22:21 +0300

    Add parser/Lesson.java

---
+++ src/main/java/parser/Lesson.java
@@ -0,0 +1,18 @@
+package parser;
+
+public class Lesson {
+    private String lessonName = null;
+    private String lessonType = null;
+    private String teacherName = null;
+    private String classroomNum = null;
+
+    private String groupName = null;
+    private String subgroupNum = null;
+
+    private String day = null;
+    private String time = null;
+    private String number = null;
+
+    int rowIndex;
+    int columnIndex;
+}
